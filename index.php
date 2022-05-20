9d215ee75de4e20d9fec27cb723ce9caeef6f5d4
DomenicoLerose85 <domenicolerose85@libero.it>
2022-05-20 16:01:12 +0200

    paragraph_&_paragraph_length_printed

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php  
-   $name = 'Domenico';
-   echo $name;
+   $paragraph = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, vel?';
+   // echo $paragraph;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Bad Words</title>
+</head>
+<body>
+   <p><?php echo $paragraph; ?> </p>
+   <h3>Paragraph Length</h3>
+   <p><?php echo strlen($paragraph); ?> </p>
+   
+</body>
+</html>
+
+
